381f33f8d42c0fda99661463db09e78ed9e97f01
loehur <loehur@gmail.com>
2022-11-18 12:33:46 +0700

    pemakaian

--- app/Views/Rekap/data.php
+++ app/Views/Rekap/data.php
@@ -2,13 +2,13 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12 pb-1">
-                Date: <b><span class="text-success"><?php echo $data['mon'][2] . "-" . $data['mon'][1] . "-" . $data['mon'][0]; ?></span></b>
+                <b><span class="text-success">Penjualan: <?php echo $data['mon'][2] . "-" . $data['mon'][1] . "-" . $data['mon'][0]; ?></span></b>
                 <table class="mt-1 table table-sm table-borderless table-striped">
                     <thead class="border">
                         <tr>
                             <th class="">Toko/Barang</th>
-                            <th class="text-right">Harga/Modal</th>
-                            <th class="text-right">Margin</th>
+                            <th class="text-right">Harga (Modal)</th>
+                            <th class="text-right">Margin (Fee)</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -18,6 +18,9 @@
                         $omset = 0;
                         $modalTotal = 0;
                         foreach ($data['data'] as $dp) {
+                            if ($dp['used'] == 1) {
+                                continue;
+                            }
                             $margin = $dp['harga_jual'] - $dp['harga'] - $dp['fee'];
                         ?>
                             <tr>
@@ -32,10 +35,43 @@
                             $modalTotal += $dp['harga'];
                         }
                         ?>
-                        <tr>
+                        <tr class="table-success">
                             <td class="text-right"></td>
                             <td class="text-right"><b><?= number_format($omset) ?> <small>(<?= number_format($modalTotal) ?>)</b></td>
                             <td class="text-right"><b><?= number_format($margin_total) ?></b> <small>(<?= number_format($fee_total) ?>)</small></td>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-12 pb-1">
+                <b><span class="text-danger">Pemakaian: <?php echo $data['mon'][2] . "-" . $data['mon'][1] . "-" . $data['mon'][0]; ?></span></b>
+                <table class="mt-1 table table-sm table-borderless table-striped">
+                    <thead class="border">
+                        <tr>
+                            <th class="">Toko/Barang</th>
+                            <th class="text-right">Biaya</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $modalTotal = 0;
+                        foreach ($data['data'] as $dp) {
+                            if ($dp['used'] == 0) {
+                                continue;
+                            }
+                        ?>
+                            <tr>
+                                <td class=""><?= strtoupper($dp['id_user'] . "#" . $dp['kode_barang']) ?></td>
+                                <td class="text-right"><?= number_format($dp['harga']) ?></td>
+                            </tr>
+                        <?php
+                            $modalTotal += $dp['harga'];
+                        }
+                        ?>
+                        <tr class="table-danger">
+                            <td class="text-right"></td>
+                            <td class="text-right"><b><?= number_format($modalTotal) ?></b></td>
                             </td>
                         </tr>
                     </tbody>
--- app/Views/RekapMonth/data.php
+++ app/Views/RekapMonth/data.php
@@ -2,15 +2,13 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12 pb-1">
-                Month: <b><span class="text-success"><?php echo $data['mon'][1] . "-" . $data['mon'][0]; ?></span></b>
+                <b><span class="text-success">Penjualan: <?= $data['mon'][1] . "-" . $data['mon'][0]; ?></span></b>
                 <table class="mt-1 table table-sm table-borderless table-striped">
                     <thead class="border">
                         <tr>
                             <th>Toko</th>
-                            <th class="text-right">Omset</th>
-                            <th class="text-right">Modal</th>
-                            <th class="text-right">Fee</th>
-                            <th class="text-right">Margin</th>
+                            <th class="text-right">Omset (Modal)</th>
+                            <th class="text-right">Margin (Fee)</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -22,6 +20,9 @@
                         $modal = 0;
 
                         foreach ($data['data'] as $dp) {
+                            if ($dp['used'] == 1) {
+                                continue;
+                            }
                             $margin = $dp['harga_jual'] - $dp['harga'] - $dp['fee'];
                         ?>
                         <?php
@@ -39,19 +40,66 @@
                         foreach ($rekap as $rekap_key => $rekap_val) { ?>
                             <tr>
                                 <td><?= strtoupper($rekap_key) ?></td>
-                                <td class="text-right"><?= number_format($rekap_val['omset']) ?></td>
-                                <td class="text-right"><?= number_format($rekap_val['modal']) ?></td>
-                                <td class="text-right"><?= number_format($rekap_val['fee_total']) ?></td>
-                                <td class="text-right"><?= number_format($rekap_val['margin_total']) ?></td>
+                                <td class="text-right"><?= number_format($rekap_val['omset']) ?><small> (<?= number_format($rekap_val['modal']) ?>)</small></td>
+                                <td class="text-right"><?= number_format($rekap_val['margin_total']) ?> <small>(<?= number_format($rekap_val['fee_total']) ?>)</small></td>
                             </tr>
                         <?php } ?>
 
+                        <tr class="table-success">
+                            <td></td>
+                            <td class="text-right"><b><?= number_format($omset) ?> <small>(<?= number_format($modal) ?>)</small></b></td>
+                            <td class="text-right"><b><?= number_format($margin_total) ?> <small>(<?= number_format($fee_total) ?>)</small></b></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-12 pb-1">
+                <b><span class="text-danger">Pemakaian: <?= $data['mon'][1] . "-" . $data['mon'][0]; ?></span></b>
+                <table class="mt-1 table table-sm table-borderless table-striped">
+                    <thead class="border">
                         <tr>
-                            <td>Total</td>
-                            <td class="text-right"><?= number_format($omset) ?></td>
-                            <td class="text-right"><?= number_format($modal) ?></td>
-                            <td class="text-right"><?= number_format($fee_total) ?></td>
-                            <td class="text-right"><?= number_format($margin_total) ?></td>
+                            <th>Toko</th>
+                            <th class="text-right">Omset (Modal)</th>
+                            <th class="text-right">Margin (Fee)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $rekap = [];
+                        $margin_total = 0;
+                        $fee_total = 0;
+                        $omset = 0;
+                        $modal = 0;
+
+                        foreach ($data['data'] as $dp) {
+                            if ($dp['used'] == 0) {
+                                continue;
+                            }
+                            $margin = $dp['harga_jual'] - $dp['harga'] - $dp['fee'];
+                        ?>
+                        <?php
+                            $margin_total += $margin;
+                            $fee_total += $dp['fee'];
+                            $omset += $dp['harga_jual'];
+                            $modal += $dp['harga'];
+
+                            $rekap[$dp['id_user']]['margin_total'] = (isset($rekap[$dp['id_user']]['margin_total'])) ? $rekap[$dp['id_user']]['margin_total'] += $margin : $margin;
+                            $rekap[$dp['id_user']]['fee_total'] = (isset($rekap[$dp['id_user']]['fee_total'])) ? $rekap[$dp['id_user']]['fee_total'] += $dp['fee'] : $dp['fee'];
+                            $rekap[$dp['id_user']]['omset'] = (isset($rekap[$dp['id_user']]['omset'])) ? $rekap[$dp['id_user']]['omset'] += $dp['harga_jual'] : $dp['harga_jual'];
+                            $rekap[$dp['id_user']]['modal'] = (isset($rekap[$dp['id_user']]['modal'])) ? $rekap[$dp['id_user']]['modal'] += $dp['harga'] : $dp['harga'];
+                        }
+
+                        foreach ($rekap as $rekap_key => $rekap_val) { ?>
+                            <tr>
+                                <td><?= strtoupper($rekap_key) ?></td>
+                                <td class="text-right"><?= number_format($rekap_val['omset']) ?><small> (<?= number_format($rekap_val['modal']) ?>)</small></td>
+                                <td class="text-right"><?= number_format($rekap_val['margin_total']) ?> <small>(<?= number_format($rekap_val['fee_total']) ?>)</small></td>
+                            </tr>
+                        <?php } ?>
+                        <tr class="table-danger">
+                            <td></td>
+                            <td class="text-right"><b><?= number_format($omset) ?> <small>(<?= number_format($modal) ?>)</small></b></td>
+                            <td class="text-right"><b><?= number_format($margin_total) ?> <small>(<?= number_format($fee_total) ?>)</small></b></td>
                         </tr>
                     </tbody>
                 </table>
