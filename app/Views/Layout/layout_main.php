559ab23cbe365b4fcdd2b67074fce2e6d2298902
loehur <loehur@gmail.com>
2022-11-21 11:06:22 +0700

    refresh

--- app/Views/Layout/layout_main.php
+++ app/Views/Layout/layout_main.php
@@ -67,3 +67,19 @@
 </body>
 
 </html>
+
+<script src="<?= $this->ASSETS_URL ?>js/jquery-3.6.0.min.js"></script>
+<script>
+	var time = new Date().getTime();
+	$(document.body).bind("mousemove keypress", function(e) {
+		time = new Date().getTime();
+	});
+
+	function refresh() {
+		if (new Date().getTime() - time >= 420000)
+			window.location.reload(true);
+		else
+			setTimeout(refresh, 10000);
+	}
+	setTimeout(refresh, 10000);
+</script>
