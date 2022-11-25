7e41d762ee161780de3e05946aee4f6fd7e48f17
loehur <47594852+loehur@users.noreply.github.com>
2022-11-25 19:01:04 +0700

    qc spsfdfdfdsa34dfd

--- app/Controllers/StokData.php
+++ app/Controllers/StokData.php
@@ -16,6 +16,40 @@ class StokData extends Controller
       $data['laris'] = $this->modul("Main")->terlaris();
 
       foreach ($data['stok'] as $d) {
+         $combine[$d['id_user']][$d['id_barang']] = $d;
+
+         foreach ($data['laris'] as $s) {
+            if ($s['id_barang'] == $d['id_barang'] && $s['id_user'] == $d['id_user']) {
+               $laku = $s['jumlah'];
+               $combine[$d['id_user']][$d['id_barang']]['laris'] = $laku;
+            }
+         }
+      }
+
+      $push = [];
+      $col_1 = 0;
+      $col_2 = 0;
+
+      foreach ($combine as $arr => $val) {
+         if ($col_1 <= $col_2) {
+            $push[1][$arr] = $val;
+            $col_1 += count($val);
+         } else {
+            $push[2][$arr] = $val;
+            $col_2 += count($val);
+         }
+      }
+
+      $this->view($this->content, $push);
+   }
+
+   function backup()
+   {
+      $this->view_layout(["title" => __CLASS__]);
+      $data['stok'] = $this->modul("Main")->list_stok_all();
+      $data['laris'] = $this->modul("Main")->terlaris();
+
+      foreach ($data['stok'] as $d) {
          foreach ($data['laris'] as $s) {
             if ($s['id_barang'] == $d['id_barang'] && $s['id_user'] == $d['id_user']) {
                $laku = $s['jumlah'];
