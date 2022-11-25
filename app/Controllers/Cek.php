e755f88025d2a2ba6dc77bb3b2b65facaeb7c1ec
loehur <47594852+loehur@users.noreply.github.com>
2022-11-25 18:41:31 +0700

    qc spsfdfd

--- app/Controllers/Cek.php
+++ app/Controllers/Cek.php
@@ -1,13 +1,13 @@
 <?php
 
-class Tes extends Controller
+class Cek extends Controller
 {
    function __construct()
    {
       $this->session()->check();
       $this->data();
    }
-   function index()
+   function laku()
    {
       $bj = $this->model("Get")->cols_where_groubBy_orderBy("barang_jual", "id_barang, id_user, SUM(jumlah) as jumlah", "id_master = '" . $this->userData['id_user'] . "' AND op_status = 1", "id_barang, id_user", "id_user ASC");
       $bp = $this->model("Get")->cols_where_groubBy_orderBy("barang_pakai", "id_barang, id_user, SUM(jumlah) as jumlah", "id_master = '" . $this->userData['id_user'] . "' AND op_status = 1", "id_barang, id_user", "id_user ASC");
