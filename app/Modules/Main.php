fc5b2714c38938872464a3da0dd2ca229eff87ba
loehur <loehur@gmail.com>
2024-08-19 17:39:31 +0700

    fix color2222

--- app/Modules/Main.php
+++ app/Modules/Main.php
@@ -78,10 +78,6 @@ class Main extends Controller
 
       $update_stok = $this->model("Insert")->cols("barang_stok", $cols, $vals);
 
-      if ($update_stok['errno'] == 0) {
-         $this->model('Log')->write($update_stok['error']);
-      }
-
       if ($update_stok['errno'] == "1062") {
          if ($rak <> "") {
             $set = "stok = " . $sisa_stok . ", rak = '" . $rak . "', en = 1";
@@ -91,6 +87,10 @@ class Main extends Controller
 
          $where = "id = '" . $id_stok . "'";
          $this->model("Update")->update("barang_stok", $set, $where);
+      } else {
+         if ($update_stok['errno'] <> 0) {
+            $this->model('Log')->write($update_stok['error']);
+         }
       }
    }
 
